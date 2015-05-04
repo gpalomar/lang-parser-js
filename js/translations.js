de02e2d7230f332a768310a8a805f2bcd5aa955c
dustdev <guillem.palomar@be12.ch>
2015-05-05 01:56:16 +0200

    Dynamical content loaded (json).

--- js/translations.js
+++ js/translations.js
@@ -1,25 +1,25 @@
 var en_sample = {
-	normal_attribute_sample: 'this is the text with normal format.',
-	upper_attribute_sample: 'this is the text with upper format.',
-	lower_attribute_sample: 'this is the text with lower format.',
-	cap_attribute_sample: 'this is the text with capitalize format.',
-	original_text_sample: 'This is the original text.',
-	translated_text_sample: 'This is the translated text.',
-	username: 'My name is Mr. X',
-	first_label: 'This my the first label test.',
+	normal_attribute_sample: "this is the text with normal format.",
+	upper_attribute_sample: "this is the text with upper format.",
+	lower_attribute_sample: "this is the text with lower format.",
+	cap_attribute_sample: "this is the text with capitalize format.",
+	original_text_sample: "This is the original text.",
+	translated_text_sample: "This is the translated text.",
+	username: "My name is Mr. X",
+	first_label: "This my the first label test.",
 	generic_button: "Translate all!",
 	container_button: "Translate content!"
 };
 
 var es_sample = {
-	normal_attribute_sample: 'este es el texto con formato normal.',
-	upper_attribute_sample: 'este es el texto con formato mayúsculas.',
-	lower_attribute_sample: 'este es el texto con formato minúsculas.',
-	cap_attribute_sample: 'este es el texto con formato capitalizado.',
-	original_text_sample: 'Este es el texto original.',
-	translated_text_sample: 'Este es el texto traducido.',
-	username: 'Mi nombre es Sr. X',
-	first_label: 'Éste es mi primera prueba con un label.',
+	normal_attribute_sample: "este es el texto con formato normal.",
+	upper_attribute_sample: "este es el texto con formato mayúsculas.",
+	lower_attribute_sample: "este es el texto con formato minúsculas.",
+	cap_attribute_sample: "este es el texto con formato capitalizado.",
+	original_text_sample: "Este es el texto original.",
+	translated_text_sample: "Este es el texto traducido.",
+	username: "Mi nombre es Sr. X",
+	first_label: "Éste es mi primera prueba con un label.",
 	generic_button: "Traduce todo!",
 	container_button: "Traduce contenido!"
 };
